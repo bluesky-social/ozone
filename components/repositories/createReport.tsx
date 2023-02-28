25ff6958be45922a33457e42a6392cfd7b224495
Aryan Goharzad <arrygoo@gmail.com>
2023-02-28 12:38:13 -0500

    Moves data fetching from AccountView to repositories

---
+++ components/repositories/createReport.tsx
@@ -0,0 +1,18 @@
+'use client'
+import client from '../../lib/client'
+import { ReportFormValues } from '../reports/ReportPanel'
+
+export async function createReport(vals: ReportFormValues) {
+  await client.api.com.atproto.report.create({
+    ...vals,
+    subject: vals.subject.startsWith('at://')
+      ? {
+          $type: 'com.atproto.repo.recordRef',
+          uri: vals.subject,
+        }
+      : {
+          $type: 'com.atproto.repo.repoRef',
+          did: vals.subject,
+        },
+  })
+}
