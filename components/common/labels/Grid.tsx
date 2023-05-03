ce1004e954a1c8ade8fe60dfcf7431d5653d3df9
Ansh Nanda <anshnanda10@gmail.com>
2023-05-03 14:55:01 -0700

    update styling of labels grid

--- components/common/labels/Grid.tsx
+++ components/common/labels/Grid.tsx
@@ -1,5 +1,5 @@
 import { Fragment } from 'react'
-import { Popover, Transition } from '@headlessui/react'
+import { Disclosure, Transition } from '@headlessui/react'
 import { useSyncedState } from '../../../lib/useSyncedState'
 import { LabelChip, LabelList, LabelListEmpty } from './List'
 import { labelOptions, displayLabel } from './util'
@@ -23,22 +23,20 @@ export function LabelsGrid(props: LabelsProps) {
   )
   const current = unpackMemo<string[]>(packedCurrent)
   return (
-    <Popover className="relative">
-      <Popover.Button
-        as={LabelList}
-        // @ts-ignore
-        disabled={disabled}
+    <Disclosure as="div">
+      <Disclosure.Button
+        as={"div"}
         className={`${disabled ? '' : 'cursor-pointer'}	${className}`}
         {...others}
       >
-        {!current.length && <LabelListEmpty>(click to add)</LabelListEmpty>}
+        {!current.length && <button>(click to add)</button>}
         {current.map((label) => (
-          <LabelChip key={label}>
+          <LabelChip key={label}> 
             {displayLabel(label)}
             <input type="hidden" name={name} value={label} />
           </LabelChip>
         ))}
-      </Popover.Button>
+      </Disclosure.Button>
       <Transition
         as={Fragment}
         enter="transition ease-out duration-200"
@@ -56,36 +54,43 @@ export function LabelsGrid(props: LabelsProps) {
           setPackedCurrent(packMemo(nextLabels))
         }}
       >
-        <Popover.Panel className="absolute left-1/2 z-10 mt-1 flex w-screen max-w-max -translate-x-1/2 px-4">
-          <div className="w-screen max-w-sm flex-auto rounded bg-white p-4 text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
-            <div className="space-y-1" id={`${id}-staged-container`}>
-              {allOptions.map((opt, i) => {
-                return (
-                  <div key={opt} className="relative flex items-start">
-                    <div className="flex h-6 items-center">
-                      <input
-                        id={`${id}-opt-${i}`}
-                        name={`${name}-staged`}
-                        type="checkbox"
-                        value={opt}
-                        defaultChecked={current.includes(opt)}
-                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                      />
-                    </div>
-                    <label
-                      htmlFor={`${id}-opt-${i}`}
-                      className="ml-3 text-sm leading-6 font-medium text-gray-900"
-                    >
-                      {displayLabel(opt)}
-                    </label>
+        <Disclosure.Panel>
+          <div
+            className="flex flex-wrap flex-row gap-3"
+            id={`${id}-staged-container`}
+          >
+            {allOptions.map((opt, i) => {
+              return (
+                <div key={opt} className="flex flex-row">
+                  <div className="flex h-6 items-center">
+                    <input
+                      id={`${id}-opt-${i}`}
+                      name={`${name}-staged`}
+                      type="checkbox"
+                      value={opt}
+                      defaultChecked={current.includes(opt)}
+                      className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                      onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                          e.preventDefault() // make sure we don't submit the form
+                          e.currentTarget.click() // simulate a click on the input
+                        }
+                      }}
+                    />
                   </div>
-                )
-              })}
-            </div>
+                  <label
+                    htmlFor={`${id}-opt-${i}`}
+                    className="ml-1 text-sm leading-6 font-medium text-gray-900"
+                  >
+                    {displayLabel(opt)}
+                  </label>
+                </div>
+              )
+            })}
           </div>
-        </Popover.Panel>
+        </Disclosure.Panel>
       </Transition>
-    </Popover>
+    </Disclosure>
   )
 }
 
