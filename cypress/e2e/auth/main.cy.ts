7b74770a3167eb1d7b3eae483177b3df88f8d520
Foysal Ahamed <cfaion341@gmail.com>
2023-06-13 16:35:54 +0200

    :white_check_mark: Add cypress test-suite and github workflow (#113)
    * :white_check_mark: Add cypress test-suite and github workflow
    
    * :broom: Cleanup unnecesary cypress code
    
    * :broom: Cleanup unnecesary cypress code

---
+++ cypress/e2e/auth/main.cy.ts
@@ -0,0 +1,83 @@
+/// <reference types="cypress" />"
+
+describe('Authentication', () => {
+  const SERVER_URL = 'https://bsky.social/xrpc/'
+
+  const mockAuthResponse = (response: Record<string, any>) =>
+    cy.intercept(
+      'POST',
+      `${SERVER_URL}/com.atproto.server.createSession`,
+      response,
+    )
+
+  const mockRepoResponse = (response: Record<string, any>) =>
+    cy.intercept('GET', `${SERVER_URL}/com.atproto.admin.getRepo*`, response)
+
+  const mockProfileResponse = (response: Record<string, any>) =>
+    cy.intercept('GET', `${SERVER_URL}/app.bsky.actor.getProfile*`, response)
+
+  const mockModerationReportsResponse = (response: Record<string, any>) =>
+    cy.intercept(
+      'GET',
+      `${SERVER_URL}/com.atproto.admin.getModerationReports*`,
+      response,
+    )
+
+  let authFixture
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+    cy.fixture('auth.json').then((data) => (authFixture = data))
+  })
+
+  it('Displays error when authentication fails', () => {
+    // Setup the auth response
+    const errorMessage = 'Invalid email'
+    mockAuthResponse({
+      statusCode: 401,
+      body: {
+        error: 'AuthenticationRequired',
+        message: errorMessage,
+      },
+    })
+
+    cy.get('#service-url').should('have.value', 'https://bsky.social')
+    cy.get('#account-handle').type('alice.test')
+    cy.get('#password').type('hunter2')
+    cy.get('#admin-token').type('admin-pass')
+    cy.get("button[type='submit']").click()
+
+    // Assert that the error message is displayed in the UI
+    cy.get('form').should('include.text', errorMessage)
+  })
+
+  it('Logs in and opens reports page when authentication succeeds', () => {
+    // Setup the auth response
+    const errorMessage = 'Invalid email'
+    mockAuthResponse({
+      statusCode: 200,
+      body: authFixture.createSessionResponse,
+    })
+    mockRepoResponse({
+      statusCode: 200,
+      body: authFixture.getRepoResponse,
+    })
+    mockProfileResponse({
+      statusCode: 200,
+      body: authFixture.getProfileResponse,
+    })
+    mockModerationReportsResponse({
+      statusCode: 200,
+      body: { cursor: null, reports: [] },
+    })
+
+    cy.get('#service-url').should('have.value', 'https://bsky.social')
+    cy.get('#account-handle').type('alice.test')
+    cy.get('#password').type('hunter2')
+    cy.get('#admin-token').type('admin-pass')
+    cy.get("button[type='submit']").click()
+
+    // Assert that the reports are displayed
+    cy.get('table').should('include.text', "Loading reports...")
+  })
+})
