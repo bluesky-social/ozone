25104b24cc88b8f1cf7395f33d549f0eefc78893
Foysal Ahamed <cfaion341@gmail.com>
2023-07-07 18:14:14 +0200

    :sparkles: Add better tracking of search keyword

--- components/reports/useFluentReportSearch.ts
+++ components/reports/useFluentReportSearch.ts
@@ -30,9 +30,12 @@ export const buildParamsFromQuery = (
 
   fragments?.forEach((fragment) => {
     allPrefixes.forEach((prefix) => {
-      if (fragment.startsWith(prefix)) {
+      if (fragment.startsWith(`${prefix}:`)) {
+        // We are doing this weird join thing because the separator between dids is also :
+        // so a search for actionedBy:did:example:etc would end up creating multiple fragments when split by :
+        // which is why, we need to only take the first fragment as the key and put the rest back together as value
         const [_, ...rest] = fragment.split(`:`)
-        params[prefix] = rest?.join(':') || ''
+        params[prefix] = rest.join(':')
       }
     })
   })
