110a83729f60e0085a6bee2e2252da6d33972631
Foysal Ahamed <cfaion341@gmail.com>
2023-08-22 14:02:09 +0200

    :broom: Cleanup old todo

--- components/repositories/AccountView.tsx
+++ components/repositories/AccountView.tsx
@@ -356,7 +356,7 @@ function Details({
 }) {
   const labels = getLabelsForSubject({ repo }).map((label) =>
     toLabelVal(label, repo.did),
-  ) // @TODO client types
+  )
   return (
     <div className="mx-auto mt-6 max-w-5xl px-4 sm:px-6 lg:px-8">
       <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2 mb-10">
