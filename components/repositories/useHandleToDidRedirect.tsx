6fcfdf25c2568416e15eec59aff773d9f873bf25
Foysal Ahamed <cfaion341@gmail.com>
2024-02-07 01:06:06 +0100

    :bug: Maintain search params when redirecting

--- components/repositories/useHandleToDidRedirect.tsx
+++ components/repositories/useHandleToDidRedirect.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useEffect, useState } from 'react'
-import { redirect } from 'next/navigation'
+import { redirect, useRouter, useSearchParams } from 'next/navigation'
 
 import { getDidFromHandle } from '@/lib/identity'
 
@@ -8,6 +8,8 @@ export const useHandleToDidRedirect = (
   handle: string,
   buildRedirectUrl: (did: string) => string,
 ) => {
+  const router = useRouter()
+  const searchParams = useSearchParams()
   const [isFetching, setIsFetching] = useState<boolean>(true)
 
   useEffect(() => {
@@ -19,8 +21,13 @@ export const useHandleToDidRedirect = (
     const fetchDidAndRedirect = async () => {
       setIsFetching(true)
       const did = await getDidFromHandle(handle)
+      const params = searchParams.toString()
       if (did) {
-        redirect(buildRedirectUrl(did))
+        let url = buildRedirectUrl(did)
+        if (params) {
+          url += `?${params}`
+        }
+        router.replace(url)
       }
       setIsFetching(false)
     }
