73c15141dcde63e6eea09d9ea9ad45e98740dff5
Foysal Ahamed <foysal@blueskyweb.xyz>
2024-04-23 14:28:15 +0200

    🧹 Cleanup unused helpers and constants around labelers (#92)
    * :sparkles: Show blob divert event
    
    * :sparkles: Use labels from 3p labeler
    
    * :recycle: Reuse existing helper for cusotm label values
    
    * :broom: Cleanup unused labels definition
    
    * :arrow_up: Upgrade atproto api version
    
    * :bug: Remove getServieDid method duplicate

--- components/common/labels/util.ts
+++ components/common/labels/util.ts
@@ -4,24 +4,9 @@ import {
   AppBskyActorDefs,
   AppBskyLabelerDefs,
   ComAtprotoLabelDefs,
+  LABELS,
   ToolsOzoneModerationDefs,
 } from '@atproto/api'
-import {
-  LabelDefinition,
-  LabelGroupDefinition,
-  LABELS,
-  LABEL_GROUPS,
-} from './data'
-
-export type LabelGroupInfoRecord = {
-  color: string
-  labels: Array<string | LabelDefinition>
-}
-
-type GroupedLabelList = Record<
-  string,
-  LabelGroupInfoRecord & Omit<LabelGroupDefinition, 'labels'>
->
 
 export type ExtendedLabelerServiceDef =
   | (AppBskyLabelerDefs.LabelerViewDetailed & {
@@ -65,119 +50,32 @@ export function toLabelVal(
   return val
 }
 
-export const labelOptions = Object.keys(LABELS)
+export const ALL_LABELS = LABELS
 
-export const LabelGroupInfo: Record<
-  string,
-  Partial<LabelGroupDefinition> & { color: string }
-> = {
-  [LABEL_GROUPS.system.id]: {
-    color: '#c45722',
-  },
-  [LABEL_GROUPS.sexual.id]: {
+export const LabelGroupInfo: Record<string, { color: string }> = {
+  [LABELS.sexual.identifier]: {
     color: '#d45722',
   },
-  [LABEL_GROUPS.violence.id]: {
+  [LABELS['graphic-media'].identifier]: {
     color: '#d42222',
   },
-  [LABEL_GROUPS.intolerance.id]: {
+  [LABELS.porn.identifier]: {
     color: '#d422bc',
   },
-  [LABEL_GROUPS.legal.id]: {
+  [LABELS.nudity.identifier]: {
     color: '#3502cc',
   },
-  [LABEL_GROUPS.rude.id]: {
-    color: '#ccb802',
-  },
-  [LABEL_GROUPS.curation.id]: {
-    color: '#ff0303',
-  },
-  [LABEL_GROUPS.misinfo.id]: {
-    color: '#530303',
-  },
-  custom: {
-    color: '#218734',
-    strings: {
-      settings: {
-        en: {
-          name: 'Custom Labels',
-          description: 'Labels from labeler service',
-        },
-      },
-    },
-  },
-  uncategorized: {
-    strings: {
-      settings: {
-        en: {
-          name: 'Uncategorzied',
-          description: 'Labels that have not been categorized yet',
-        },
-      },
-    },
-    color: '',
-    labels: [],
-  },
 }
 
-const labelGroupsRequiringBlur = [
-  LABEL_GROUPS.violence.id,
-  LABEL_GROUPS.sexual.id,
+const labelsRequiringBlur = [
+  LABELS['graphic-media'].identifier,
+  LABELS.porn.identifier,
+  LABELS.nudity.identifier,
+  LABELS.sexual.identifier,
 ]
 
-export const groupLabelList = (labels: string[]): GroupedLabelList => {
-  const groupedList: GroupedLabelList = {}
-  const customLabels = getCustomLabels()
-
-  labels.forEach((label) => {
-    // SELF_FLAG is embedded in the label value so when grouping, we have to take it out of the value
-    const cleanedLabel = unFlagSelfLabel(label)
-    const group = LABELS[cleanedLabel]
-    let groupId = 'uncategorized'
-
-    if (group?.groupId) {
-      groupId = group.groupId
-    } else if (customLabels?.includes(cleanedLabel)) {
-      groupId = 'custom'
-    }
-
-    if (groupedList[groupId]) {
-      groupedList[groupId].labels.push(label)
-    } else {
-      groupedList[groupId] = {
-        ...(LabelGroupInfo[groupId] || LabelGroupInfo.uncategorized),
-        ...(LABEL_GROUPS[groupId] || {}),
-        labels: [label],
-      }
-    }
-  })
-
-  return groupedList
-}
-
-const getCustomLabels = () =>
-  client.session?.config.labeler?.policies.labelValues
-
-export const getLabelGroupInfo = (label: string): LabelGroupInfoRecord => {
-  const customLabels = getCustomLabels()
-  const group = LABELS[label]
-  const groupId =
-    group?.groupId ||
-    (customLabels?.includes(label) ? 'custom' : 'uncategorized')
-
-  return {
-    // TODO: We shouldn't have to do this, there's a weird type def somewhere that's causing this
-    labels: [],
-    ...LabelGroupInfo.uncategorized,
-    ...(LabelGroupInfo[groupId] || {}),
-    ...(group || {}),
-  }
-}
-
 export const doesLabelNeedBlur = (labels?: string[]): boolean =>
-  !!labels?.find((label) =>
-    labelGroupsRequiringBlur.includes(LABELS[label]?.groupId),
-  )
+  !!labels?.find((label) => labelsRequiringBlur.includes(label))
 
 export const doesProfileNeedBlur = ({
   profile,
@@ -205,6 +103,9 @@ export const getLabelsForSubject = ({
 }) => {
   return record?.labels ?? repo?.labels ?? []
 }
+
+export const getCustomLabels = () =>
+  client.session?.config.labeler?.policies.labelValues || []
 
 export const buildAllLabelOptions = (
   defaultLabels: string[],
