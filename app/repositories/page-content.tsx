58bb7114362f830b8ee8befbf49cdeb421d17e90
Foysal Ahamed <foysal@blueskyweb.xyz>
2024-06-18 22:42:01 +0200

    :sparkles: Show search results when searching by email: prefix

--- app/repositories/page-content.tsx
+++ app/repositories/page-content.tsx
@@ -4,6 +4,71 @@ import { useSearchParams } from 'next/navigation'
 import { useInfiniteQuery } from '@tanstack/react-query'
 import client from '@/lib/client'
 import { useTitle } from 'react-use'
+import { ToolsOzoneModerationDefs } from '@atproto/api'
+
+const isEmailSearch = (q: string) => q.startsWith('email:')
+
+const getSearchResults = async ({
+  q,
+  cursor,
+}: {
+  q: string
+  cursor?: string
+}): Promise<{
+  cursor?: string
+  repos: ToolsOzoneModerationDefs.RepoView[]
+}> => {
+  const headers = { headers: client.proxyHeaders() }
+  const limit = 25
+
+  if (!isEmailSearch(q)) {
+    const { data } = await client.api.tools.ozone.moderation.searchRepos(
+      { q, limit, cursor },
+      headers,
+    )
+
+    return data
+  }
+
+  const email = q.replace('email:', '').trim()
+
+  if (!email) {
+    return { repos: [], cursor: undefined }
+  }
+
+  const { data } = await client.api.com.atproto.admin.searchAccounts(
+    { email, limit, cursor },
+    headers,
+  )
+
+  if (!data.accounts.length) {
+    return { repos: [], cursor: data.cursor }
+  }
+
+  const repos: Record<string, ToolsOzoneModerationDefs.RepoView> = {}
+  data.accounts.forEach((account) => {
+    repos[account.did] = {
+      ...account,
+      // Set placeholder properties that will be later filled in with data from ozone
+      relatedRecords: [],
+      indexedAt: account.indexedAt,
+      moderation: {},
+      labels: [],
+    }
+  })
+
+  await Promise.allSettled(
+    data.accounts.map(async (account) => {
+      const { data } = await client.api.tools.ozone.moderation.getRepo(
+        { did: account.did },
+        headers,
+      )
+      repos[account.did] = { ...repos[account.did], ...data }
+    }),
+  )
+
+  return { repos: Object.values(repos), cursor: data.cursor }
+}
 
 export default function RepositoriesListPage() {
   const params = useSearchParams()
@@ -11,15 +76,7 @@ export default function RepositoriesListPage() {
   const { data, fetchNextPage, hasNextPage } = useInfiniteQuery({
     queryKey: ['repositories', { q }],
     queryFn: async ({ pageParam }) => {
-      const { data } = await client.api.tools.ozone.moderation.searchRepos(
-        {
-          q,
-          limit: 25,
-          cursor: pageParam,
-        },
-        { headers: client.proxyHeaders() },
-      )
-      return data
+      return getSearchResults({ q, cursor: pageParam })
     },
     getNextPageParam: (lastPage) => lastPage.cursor,
   })
@@ -38,6 +95,7 @@ export default function RepositoriesListPage() {
 
       <RepositoriesTable
         repos={repos}
+        showEmail={isEmailSearch(q)}
         onLoadMore={fetchNextPage}
         showLoadMore={!!hasNextPage}
         showEmptySearch={!q?.length && !repos.length}
