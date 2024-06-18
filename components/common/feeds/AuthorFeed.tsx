3109f461ef67ae62ce6bf88e1c81547aa5bb25eb
Foysal Ahamed <foysal@blueskyweb.xyz>
2024-06-18 19:46:48 +0200

    :sparkles: Add Repost, quote, media and no reply filters to author feed

--- components/common/feeds/AuthorFeed.tsx
+++ components/common/feeds/AuthorFeed.tsx
@@ -4,6 +4,66 @@ import { Posts } from '../posts/Posts'
 import client from '@/lib/client'
 import { useState } from 'react'
 import { useRepoAndProfile } from '@/repositories/useRepoAndProfile'
+import { AppBskyFeedDefs } from '@atproto/api'
+import { TypeFilterKey, TypeFiltersByKey } from '../posts/constants'
+
+const getAuthorFeed = async ({ id, pageParam, typeFilter, options }) => {
+  const limit = 30
+  let filteredFeed: AppBskyFeedDefs.FeedViewPost[] = []
+  let cursor = pageParam
+  const isPostFilter = [
+    TypeFiltersByKey.posts_no_replies.key,
+    TypeFiltersByKey.posts_with_media.key,
+  ].includes(typeFilter)
+  const isQuoteOrRepostFilter = [
+    TypeFiltersByKey.reposts.key,
+    TypeFiltersByKey.quotes.key,
+    TypeFiltersByKey.quotes_and_reposts.key,
+  ].includes(typeFilter)
+
+  while (filteredFeed.length < limit) {
+    const { data } = await client.api.app.bsky.feed.getAuthorFeed(
+      {
+        limit,
+        actor: id,
+        cursor,
+        ...(isPostFilter ? { filter: typeFilter } : {}),
+      },
+      options,
+    )
+
+    // Only repost/quote post filters are applied on the client side
+    if (!isQuoteOrRepostFilter) {
+      return data
+    }
+
+    const newFilteredItems = data.feed.filter((item) => {
+      const isRepost = item.reason?.$type === 'app.bsky.feed.defs#reasonRepost'
+      const isQuotePost =
+        item.post.embed?.$type === 'app.bsky.embed.record#view'
+      if (typeFilter === TypeFiltersByKey.reposts.key) {
+        return isRepost
+      }
+      if (typeFilter === TypeFiltersByKey.quotes.key) {
+        // When a quoted post is reposted, we don't want to consider that a quote post
+        return isQuotePost && !isRepost
+      }
+      return isRepost || isQuotePost
+    })
+
+    filteredFeed = [...filteredFeed, ...newFilteredItems]
+
+    // If no more items are available, break the loop to prevent infinite requests
+    if (!data.cursor) {
+      break
+    }
+
+    cursor = data.cursor
+  }
+
+  // Ensure the feed is exactly 30 items if there are more than 30
+  return { feed: filteredFeed, cursor }
+}
 
 export function AuthorFeed({
   id,
@@ -13,9 +73,10 @@ export function AuthorFeed({
   onReport: (uri: string) => void
 }) {
   const [query, setQuery] = useState('')
+  const [typeFilter, setTypeFilter] = useState<TypeFilterKey>('no_filter')
   const { data: repoData } = useRepoAndProfile({ id })
   const { data, fetchNextPage, hasNextPage, isFetching } = useInfiniteQuery({
-    queryKey: ['authorFeed', { id, query }],
+    queryKey: ['authorFeed', { id, query, typeFilter }],
     queryFn: async ({ pageParam }) => {
       const options = { headers: client.proxyHeaders() }
       const searchPosts = query.length && repoData?.repo.handle
@@ -30,14 +91,12 @@ export function AuthorFeed({
         )
         return { ...data, feed: data.posts.map((post) => ({ post })) }
       } else {
-        const { data } = await client.api.app.bsky.feed.getAuthorFeed(
-          {
-            actor: id,
-            limit: 30,
-            cursor: pageParam,
-          },
+        const data = await getAuthorFeed({
+          id,
+          pageParam,
+          typeFilter,
           options,
-        )
+        })
         return data
       }
     },
@@ -48,10 +107,13 @@ export function AuthorFeed({
   return (
     <Posts
       items={items}
+      searchQuery={query}
       setSearchQuery={setQuery}
       onReport={onReport}
       isFetching={isFetching}
       onLoadMore={hasNextPage ? () => fetchNextPage() : undefined}
+      typeFilter={typeFilter}
+      setTypeFilter={setTypeFilter}
     />
   )
 }
