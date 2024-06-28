9c5461cc5941a3a56fb2a1b42907bc2695d9b3e6
Foysal Ahamed <foysal@blueskyweb.xyz>
2024-06-28 21:18:55 +0200

    :sparkles: Add typeahead search in cmd palette

--- components/shell/CommandPalette/useAsyncSearch.tsx
+++ components/shell/CommandPalette/useAsyncSearch.tsx
@@ -1,6 +1,7 @@
 import { getDidFromHandle } from '@/lib/identity'
 import { CollectionId, getCollectionName } from '@/reports/helpers/subject'
-import { AtUri } from '@atproto/api'
+import { useSearchActorsTypeahead } from '@/repositories/useSearchActorsTypeahead'
+import { AppBskyActorDefs, AtUri } from '@atproto/api'
 import {
   ChatBubbleLeftIcon,
   UserGroupIcon,
@@ -13,6 +14,7 @@ import {
   useRegisterActions,
   createAction,
   ActionSection,
+  KBarQuery,
 } from 'kbar'
 import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime'
 import { useRouter } from 'next/navigation'
@@ -88,6 +90,29 @@ const buildItemForDid = ({
   return actions
 }
 
+const buildItemFromTypeahead = ({
+  search,
+  actors = [],
+  kBarQuery,
+}: Omit<ItemBuilderProps, 'profileKey' | 'type' | 'router'> & {
+  actors: AppBskyActorDefs.ProfileViewBasic[]
+  kBarQuery: KBarQuery
+}): Action[] => {
+  return actors.map((actor) => ({
+    id: `profile-${actor.did}`,
+    name: `@${actor.handle}`,
+    keywords: `${search},actor,${actor.displayName}, ${actor.handle}`,
+    icon: <RepoIcon className={iconClassName} />,
+    subtitle: `${actor.displayName || actor.did}`,
+    section: ActionSections.actions,
+    priority: 1,
+    perform: () => {
+      kBarQuery.setSearch(actor.did)
+      kBarQuery.toggle()
+    },
+  }))
+}
+
 const buildItemForProfile = ({
   type,
   search,
@@ -151,9 +176,12 @@ const buildItemForProfile = ({
 
 export const useCommandPaletteAsyncSearch = () => {
   const router = useRouter()
-  const { search } = useKBar<{ search: string }>((state) => ({
+  const { search, query: kBarQuery } = useKBar<{ search: string }>((state) => ({
     search: state.searchQuery,
   }))
+
+  const { data: typeaheadResults } = useSearchActorsTypeahead(search)
+
   const [didFromHandle, setDidFromHandle] = useState<{
     did: string
     handle: string
@@ -366,6 +394,14 @@ export const useCommandPaletteAsyncSearch = () => {
           profileKey: search,
         }),
       )
+    } else if (search.startsWith('@')) {
+      actions.push(
+        ...buildItemFromTypeahead({
+          search,
+          actors: typeaheadResults || [],
+          kBarQuery,
+        }),
+      )
     } else if (isValidHandle(search)) {
       actions.push(
         ...buildItemForProfile({
@@ -389,7 +425,7 @@ export const useCommandPaletteAsyncSearch = () => {
     }
 
     return actions.map(createAction)
-  }, [search, didFromHandle])
+  }, [search, didFromHandle, typeaheadResults])
 
   useRegisterActions(memoizedActions, [search, didFromHandle])
 }
