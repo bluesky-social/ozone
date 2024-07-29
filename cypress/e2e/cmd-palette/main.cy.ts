314015259808d518c574cc96a14c9330582ca1c4
Foysal Ahamed <foysal@blueskyweb.xyz>
2024-07-29 12:54:36 +0200

    :white_check_mark: Add video artifacts

--- cypress/e2e/cmd-palette/main.cy.ts
+++ cypress/e2e/cmd-palette/main.cy.ts
@@ -103,6 +103,7 @@ describe('Command Palette', () => {
     // Setup the auth response
     openCommandPalette(bskyPostUrlWithHandle)
     cy.get('#kbar-listbox-item-1').contains('Take action on Post').click()
+    cy.wait(500)
     cy.location('href').then((href) => {
       expect(decodeURIComponent(href)).to.include(
         `quickOpen=at://did:plc:56ud7t6bqdkwblmzwmkcetst/app.bsky.feed.post/3kozf56ocx32a`,
@@ -112,6 +113,7 @@ describe('Command Palette', () => {
     cy.wait(1000)
     openCommandPalette(bskyPostUrlWithHandle)
     cy.get('#kbar-listbox-item-2').contains('Take action on alice.test').click()
+    cy.wait(500)
     cy.location('href').then((href) => {
       expect(decodeURIComponent(href)).to.include(
         `quickOpen=did:plc:56ud7t6bqdkwblmzwmkcetst`,
