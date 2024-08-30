d3944a41ca1dddaf45fcbb973961677be5157c4e
Matthieu Sieben <matthieu.sieben@gmail.com>
2024-08-30 15:46:25 +0200

    remove dev OZONE_PUBLIC_URL

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,11 +4,7 @@ export const OZONE_SERVICE_DID =
   process.env.NEXT_PUBLIC_OZONE_SERVICE_DID || undefined
 
 export const OZONE_PUBLIC_URL =
-  process.env.NEXT_PUBLIC_OZONE_PUBLIC_URL ||
-  (process.env.NODE_ENV === 'development'
-    ? 'http://localhost:2587'
-    : // Defaults to "window.location.origin"
-      undefined)
+  process.env.NEXT_PUBLIC_OZONE_PUBLIC_URL || undefined
 
 export const PLC_DIRECTORY_URL =
   process.env.NEXT_PUBLIC_PLC_DIRECTORY_URL ||
