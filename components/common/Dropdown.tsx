240a87ebddd53cf563acfa00bcae1917df801eac
Foysal Ahamed <foysal@blueskyweb.xyz>
2024-09-11 20:46:06 +0200

    :sparkles: Add embed type filter for moderation queue

--- components/common/Dropdown.tsx
+++ components/common/Dropdown.tsx
@@ -46,7 +46,7 @@ export const Dropdown = ({
           <Menu.Items
             className={classNames(
               rightAligned ? 'right-0' : '',
-              'absolute z-10 mt-2 w-48 origin-top-right rounded-md bg-white dark:bg-slate-800 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none',
+              'absolute z-10 mt-2 w-48 origin-top-right rounded-md bg-white dark:bg-slate-800 py-1 shadow-lg dark:shadow-slate-900 ring-1 ring-black ring-opacity-5 focus:outline-none',
             )}
           >
             {items.map((item) => (
