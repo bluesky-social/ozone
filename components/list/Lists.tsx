46c7db8f955a042e5cda7bfe3546ae3f1b10441f
Foysal Ahamed <foysal@blueskyweb.xyz>
2024-10-17 10:12:08 +0200

    :sparkles: Allow adding all lists of a user to workspace

--- components/list/Lists.tsx
+++ components/list/Lists.tsx
@@ -1,17 +1,25 @@
+import { ActionButton } from '@/common/buttons'
 import { LabelChip, ModerationLabel } from '@/common/labels'
 import { Loading } from '@/common/Loader'
+import { ConfirmationModal } from '@/common/modals/confirmation'
 import { buildBlueSkyAppUrl } from '@/lib/util'
 import { useLabelerAgent } from '@/shell/ConfigurationContext'
+import { useWorkspaceAddItemsMutation } from '@/workspace/hooks'
 import { useInfiniteQuery } from '@tanstack/react-query'
 import Link from 'next/link'
+import { useState } from 'react'
+import { toast } from 'react-toastify'
 
 export function Lists({ actor }: { actor: string }) {
   const labelerAgent = useLabelerAgent()
+  const [isConfirmationOpen, setIsConfirmationOpen] = useState(false)
+  const [isAdding, setIsAdding] = useState(false)
+  const { mutateAsync: addItemsToWorkspace } = useWorkspaceAddItemsMutation()
 
   const { data, isLoading } = useInfiniteQuery({
     queryKey: ['lists', { actor }],
     queryFn: async ({ pageParam }) => {
-      const { data } = await labelerAgent.api.app.bsky.graph.getLists({
+      const { data } = await labelerAgent.app.bsky.graph.getLists({
         actor,
         limit: 25,
         cursor: pageParam,
@@ -20,6 +28,37 @@ export function Lists({ actor }: { actor: string }) {
     },
     getNextPageParam: (lastPage) => lastPage.cursor,
   })
+
+  const lists = data?.pages.flatMap((page) => page.lists)
+
+  const confirmAddToWorkspace = async () => {
+    // add items that are already loaded
+    if (!lists?.length) return
+    await addItemsToWorkspace(lists.map((f) => f.uri))
+    if (!data?.pageParams) {
+      setIsConfirmationOpen(false)
+      return
+    }
+    setIsAdding(true)
+
+    try {
+      let cursor = data.pageParams[0] as string | undefined
+      do {
+        const nextLists = await labelerAgent.app.bsky.graph.getLists({
+          actor,
+          limit: 25,
+          cursor,
+        })
+        await addItemsToWorkspace(nextLists.data.lists.map((f) => f.uri))
+        cursor = nextLists.data.cursor
+        //   if the modal is closed, that means the user decided not to add any more user to workspace
+      } while (cursor && isConfirmationOpen)
+    } catch (e) {
+      toast.error(`Something went wrong: ${(e as Error).message}`)
+    }
+    setIsAdding(false)
+    setIsConfirmationOpen(false)
+  }
 
   if (isLoading) {
     return (
@@ -37,12 +76,47 @@ export function Lists({ actor }: { actor: string }) {
     )
   }
 
-  const lists = data.pages.flatMap((page) => page.lists)
-
   return (
     <div className="mx-auto mt-8 max-w-5xl px-4 pb-12 sm:px-6 lg:px-8">
+      <div>
+        {!!lists?.length && (
+          <div className="mt-1 flex justify-end pb-4 text-gray-900 dark:text-gray-200">
+            <ActionButton
+              appearance="primary"
+              size="sm"
+              onClick={() => setIsConfirmationOpen(true)}
+            >
+              Add to workspace
+            </ActionButton>
+
+            <ConfirmationModal
+              onConfirm={() => {
+                if (isAdding) {
+                  setIsAdding(false)
+                  setIsConfirmationOpen(false)
+                  return
+                }
+
+                confirmAddToWorkspace()
+              }}
+              isOpen={isConfirmationOpen}
+              setIsOpen={setIsConfirmationOpen}
+              confirmButtonText={isAdding ? 'Stop adding' : 'Yes, add all'}
+              title={`Add lists to workspace?`}
+              description={
+                <>
+                  Once confirmed, all the lists of the user will be added to the
+                  workspace. For users with a lot of lists, this may take quite
+                  some time but you can always stop the process and already
+                  added lists will remain in the workspace.
+                </>
+              }
+            />
+          </div>
+        )}
+      </div>
       <div className="mt-1 grid grid-cols-1 gap-4 sm:grid-cols-2 text-gray-900 dark:text-gray-200">
-        {lists.map((list) => (
+        {lists?.map((list) => (
           <div
             key={list.uri}
             className="relative rounded-lg border border-gray-300 dark:border-slate-700 bg-white dark:bg-slate-800 p-3 shadow-sm dark:shadow-slate-800 focus-within:ring-2 focus-within:ring-pink-500 focus-within:ring-teal-500 focus-within:ring-offset-2 hover:border-gray-400 dark:hover:border-slate-700"
