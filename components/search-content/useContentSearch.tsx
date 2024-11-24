52c78d462700babe5e7b5f1be4a6703d8f4b695c
Foysal Ahamed <foysal@blueskyweb.xyz>
2024-11-24 11:19:56 +0000

    :sparkles: Add people tab on search page (#247)

--- components/search-content/useContentSearch.tsx
+++ components/search-content/useContentSearch.tsx
@@ -1,22 +1,53 @@
-import { useLabelerAgent } from '@/shell/ConfigurationContext'
+import { useAppviewAgent, useLabelerAgent } from '@/shell/ConfigurationContext'
 import { useWorkspaceAddItemsMutation } from '@/workspace/hooks'
+import {
+  AppBskyActorDefs,
+  AppBskyActorSearchActors,
+  AppBskyFeedDefs,
+  AppBskyFeedSearchPosts,
+} from '@atproto/api'
 import { useInfiniteQuery } from '@tanstack/react-query'
+
+type ActorResult = AppBskyActorSearchActors.OutputSchema
+type PostResult = AppBskyFeedSearchPosts.OutputSchema
+export type SearchContentSection = 'people' | 'top' | 'latest'
+type QueryData<S extends SearchContentSection> = S extends 'people'
+  ? ActorResult
+  : PostResult
 
 export const useContentSearch = ({
   term,
   section,
 }: {
   term: string
-  section: string
+  section: SearchContentSection
 }) => {
   const labelerAgent = useLabelerAgent()
+  const appviewAgent = useAppviewAgent()
 
   const { mutate: addToWorkspace } = useWorkspaceAddItemsMutation()
 
-  const searchInfo = useInfiniteQuery({
+  const searchInfo = useInfiniteQuery<QueryData<typeof section>>({
     queryKey: ['searchContent', { term, section }],
     enabled: !!term,
     queryFn: async ({ pageParam }) => {
+      if (section === 'people') {
+        // This is actually never hit, we always have appview configured
+        if (!appviewAgent) {
+          throw new Error(
+            'Can not search actors without appview agent configured',
+          )
+        }
+
+        const { data } = await appviewAgent.app.bsky.actor.searchActors({
+          q: term,
+          limit: 30,
+          cursor: pageParam,
+        })
+
+        return data
+      }
+
       const { data } = await labelerAgent.app.bsky.feed.searchPosts({
         q: term,
         limit: 30,
@@ -28,11 +59,24 @@ export const useContentSearch = ({
     getNextPageParam: (lastPage) => lastPage.cursor,
   })
 
-  const posts = searchInfo.data?.pages.flatMap((page) => page.posts) || []
+  const data =
+    (section === 'people'
+      ? (searchInfo.data?.pages.flatMap(
+          (page) => page.actors,
+        ) as ActorResult['actors'])
+      : (searchInfo.data?.pages.flatMap(
+          (page) => page.posts,
+        ) as PostResult['posts'])) || []
 
   return {
     ...searchInfo,
-    posts,
+    data,
     addToWorkspace,
   }
+}
+
+export function isActorData(
+  data: ActorResult['actors'] | PostResult['posts'],
+): data is ActorResult['actors'] {
+  return data.length === 0 || 'did' in data[0]
 }
