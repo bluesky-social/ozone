66d04fa7f12e72f94e41952fcb04bb751c8aa082
gcwill70 <gcwill70@gmail.com>
2026-01-20 14:43:37 -0800

    refactor: update ImportMacroModal to use ConfirmationModal

--- components/mod-event/ImportMacroModal.tsx
+++ components/mod-event/ImportMacroModal.tsx
@@ -1,15 +1,8 @@
-import {
-  Description,
-  Dialog,
-  DialogPanel,
-  DialogTitle,
-  TransitionChild,
-} from '@headlessui/react'
-import { Fragment, useState } from 'react'
+import { useState } from 'react'
 import { toast } from 'react-toastify'
 
-import { ActionButton } from '@/common/buttons'
 import { Input, Textarea } from '@/common/forms'
+import { ConfirmationModal } from '@/common/modals/confirmation'
 import { useFilterMacroUpsertMutation } from '@/mod-event/useFilterMacrosList'
 import { EventListState } from './useModEventList'
 
@@ -23,17 +16,17 @@ export const ImportMacroModal = ({
   const { mutateAsync: upsertMacro } = useFilterMacroUpsertMutation()
 
   const [isImporting, setIsImporting] = useState(false)
+  const [name, setName] = useState('')
+  const [items, setItems] = useState('')
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleConfirm = async () => {
     try {
       setIsImporting(true)
-      e.preventDefault()
-      const formData = new FormData(e.currentTarget)
-      const name = formData.get('name') as string
-      const items = formData.get('items') as string
       const filters = JSON.parse(items) as Partial<EventListState>
       await upsertMacro({ name, filters })
       setIsDialogOpen(false)
+      setName('')
+      setItems('')
     } catch (err) {
       console.error('Error importing macro:', err)
       toast.error(
@@ -45,89 +38,33 @@ export const ImportMacroModal = ({
   }
 
   return (
-    <Dialog
-      as="div"
-      className="relative z-10"
-      open={isOpen}
-      onClose={() => setIsDialogOpen(false)}
+    <ConfirmationModal
+      isOpen={isOpen}
+      setIsOpen={setIsDialogOpen}
+      title="Import Macro"
+      onConfirm={handleConfirm}
+      confirmButtonDisabled={isImporting}
+      confirmButtonText="Import"
     >
-      <TransitionChild
-        as={Fragment}
-        enter="ease-out duration-300"
-        enterFrom="opacity-0"
-        enterTo="opacity-100"
-        leave="ease-in duration-200"
-        leaveFrom="opacity-100"
-        leaveTo="opacity-0"
-      >
-        <div className="fixed inset-0 bg-black bg-opacity-25" />
-      </TransitionChild>
-
-      <div className="fixed inset-0 overflow-y-auto">
-        <div className="flex min-h-full items-center justify-center p-4 text-center">
-          <TransitionChild
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0 scale-95"
-            enterTo="opacity-100 scale-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100 scale-100"
-            leaveTo="opacity-0 scale-95"
-          >
-            <DialogPanel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white dark:bg-slate-800 p-6 text-left align-middle shadow-xl transition-all">
-              <DialogTitle
-                as="h3"
-                className="text-lg font-medium leading-6 text-gray-900 dark:text-gray-50"
-              >
-                Import Macro
-              </DialogTitle>
-
-              <Description className="text-gray-600 dark:text-gray-50 mt-4">
-                <form
-                  onSubmit={handleSubmit}
-                  className="flex flex-col items-center gap-2"
-                >
-                  <Input
-                    autoFocus
-                    disabled={isImporting}
-                    name="name"
-                    placeholder="Name"
-                    className={`block p-2 w-full`}
-                    type="text"
-                  />
-                  <Textarea
-                    disabled={isImporting}
-                    name="items"
-                    placeholder={`${JSON.stringify(
-                      { types: ['appeal'] },
-                      null,
-                      1,
-                    )}`}
-                    className={`block p-2 w-full`}
-                    rows={5}
-                  />
-                  <div>
-                    <ActionButton
-                      appearance="outlined"
-                      className="mr-2"
-                      onClick={() => setIsDialogOpen(false)}
-                    >
-                      Cancel
-                    </ActionButton>
-                    <ActionButton
-                      type="submit"
-                      appearance="primary"
-                      disabled={isImporting}
-                    >
-                      Import
-                    </ActionButton>
-                  </div>
-                </form>
-              </Description>
-            </DialogPanel>
-          </TransitionChild>
-        </div>
+      <div className="flex flex-col gap-2 mt-4">
+        <Input
+          autoFocus
+          disabled={isImporting}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Name"
+          className="block p-2 w-full"
+          type="text"
+        />
+        <Textarea
+          disabled={isImporting}
+          value={items}
+          onChange={(e) => setItems(e.target.value)}
+          placeholder={`${JSON.stringify({ types: ['appeal'] }, null, 1)}`}
+          className="block p-2 w-full"
+          rows={5}
+        />
       </div>
-    </Dialog>
+    </ConfirmationModal>
   )
 }
