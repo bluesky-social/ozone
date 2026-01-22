6a22bf3435cefe1415dc29c794a009cb18312403
gcwill70 <gcwill70@gmail.com>
2026-01-21 21:15:17 -0800

    feat: use mod event context to get subject options for action panel

--- app/events/page-content.tsx
+++ app/events/page-content.tsx
@@ -1,20 +1,18 @@
-import { useTitle } from 'react-use'
+import { useWorkspaceOpener } from '@/common/useWorkspaceOpener'
+import { validSubjectString } from '@/lib/types'
+import { unique } from '@/lib/util'
 import { ModEventList } from '@/mod-event/EventList'
-import { useModEventList } from '@/mod-event/useModEventList'
 import {
   ActionPanelNames,
   hydrateModToolInfo,
   useEmitEvent,
 } from '@/mod-event/helpers/emitEvent'
+import { useModEventContext } from '@/mod-event/ModEventContext'
+import { WorkspacePanel } from '@/workspace/Panel'
 import { ToolsOzoneModerationEmitEvent } from '@atproto/api'
 import { ModActionPanelQuick } from 'app/actions/ModActionPanel/QuickAction'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
-import { WorkspacePanel } from '@/workspace/Panel'
-import { useWorkspaceOpener } from '@/common/useWorkspaceOpener'
-import { unique } from '@/lib/util'
-import { validSubjectString } from '@/lib/types'
-
-const REFETCH_INTERVAL = 10 * 1000
+import { useTitle } from 'react-use'
 
 export default function EventListPageContent() {
   const emitEvent = useEmitEvent()
@@ -35,11 +33,9 @@ export default function EventListPageContent() {
   const { toggleWorkspacePanel, isWorkspaceOpen } = useWorkspaceOpener()
 
   // Get subjects
-  const { modEvents } = useModEventList({
-    queryOptions: { refetchInterval: REFETCH_INTERVAL },
-  })
+  const { modEvents } = useModEventContext()
   const subjectOptions = unique(
-    modEvents.flatMap((report) => validSubjectString(report.subject) ?? []),
+    modEvents.flatMap((events) => validSubjectString(events.subject) ?? []),
   )
 
   useTitle(`Moderation Events`)
@@ -47,7 +43,7 @@ export default function EventListPageContent() {
   return (
     <div>
       <div className="w-5/6 sm:w-3/4 md:w-2/3 lg:w-1/2 mx-auto my-4 dark:text-gray-100">
-        <ModEventList queryOptions={{ refetchInterval: REFETCH_INTERVAL }} />
+        <ModEventList queryOptions={{ refetchInterval: 10 * 1000 }} global={true} />
       </div>
       <ModActionPanelQuick
         open={!!quickOpenParam}
