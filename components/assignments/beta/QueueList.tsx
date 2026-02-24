6c98b7cf70e27fd0773f4fab39b40c8c059c5b12
gcwill70 <gcwill70@gmail.com>
2026-02-24 15:04:43 -0500

    use fixed number of queues

--- components/assignments/beta/QueueList.tsx
+++ components/assignments/beta/QueueList.tsx
@@ -1,15 +1,9 @@
 'use client'
 
-import { useState } from 'react'
-import { ActionButton } from '@/common/buttons'
 import { QueueAssigneeStatus } from '../QueueAssigneeStatus'
 
 export function QueueList() {
-  const [queues, setQueues] = useState<number[]>([1])
-
-  const addQueue = () => {
-    setQueues((prev) => [...prev, (prev[prev.length - 1] ?? 0) + 1])
-  }
+  const queues = Array.from({ length: 10 }, (_, i) => i + 1)
 
   return (
     <div>
@@ -17,9 +11,6 @@ export function QueueList() {
         <h2 className="text-lg font-medium text-gray-900 dark:text-gray-100">
           Queues
         </h2>
-        <ActionButton appearance="primary" size="sm" onClick={addQueue}>
-          Add Queue
-        </ActionButton>
       </div>
       <div className="space-y-3">
         {queues.map((queueId) => (
