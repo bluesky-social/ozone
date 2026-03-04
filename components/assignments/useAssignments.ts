d7ea0ad6a310b7be43f71bc2c0913a802b828d2b
gcwill70 <gcwill70@gmail.com>
2026-03-04 11:53:56 -0500

    cleanup based on new API changes

--- components/assignments/useAssignments.ts
+++ components/assignments/useAssignments.ts
@@ -4,50 +4,29 @@ import { useLabelerAgent } from '@/shell/ConfigurationContext'
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import { toast } from 'react-toastify'
 import { MINUTE } from '@/lib/util'
+import {
+  ToolsOzoneQueueAssignModerator,
+  ToolsOzoneQueueDefs,
+  ToolsOzoneQueueGetAssignments,
+  ToolsOzoneReportAssignModerator,
+  ToolsOzoneReportDefs,
+  ToolsOzoneReportGetAssignments,
+  ToolsOzoneReportUnassignModerator,
+} from '@atproto/api'
 
 const ASSIGNMENTS_QUERY_KEY = 'assignments'
 
-export interface AssignmentView {
-  id: number
-  did: string
-  queueId: number
-  reportId?: number
-  startAt: string
-  endAt: string
-}
-
-export const useQueueAssignments = (params: {
-  onlyActiveAssignments?: boolean
-  queueIds?: number[]
-  dids?: string[]
-}) => {
+export const useQueueAssignments = (
+  params: ToolsOzoneQueueGetAssignments.QueryParams,
+) => {
   const labelerAgent = useLabelerAgent()
   return useQuery({
     queryKey: [ASSIGNMENTS_QUERY_KEY, params],
     queryFn: async () => {
       const { data } =
         await labelerAgent.tools.ozone.queue.getAssignments(params)
-      return (data as { assignments: AssignmentView[] }).assignments
-    },
-    refetchInterval: 5000,
-    onError: (err) => {
-      toast.error(`Failed to load assignments:\n${err}`)
-    },
-  })
-}
-
-export const useReportAssignments = (params: {
-  reportIds?: number[]
-  onlyActiveAssignments?: boolean
-  dids?: string[]
-}) => {
-  const labelerAgent = useLabelerAgent()
-  return useQuery({
-    queryKey: [ASSIGNMENTS_QUERY_KEY, params],
-    queryFn: async () => {
-      const { data } =
-        await labelerAgent.tools.ozone.report.getAssignments(params)
-      return (data as { assignments: AssignmentView[] }).assignments
+      return (data as { assignments: ToolsOzoneQueueDefs.AssignmentView[] })
+        .assignments
     },
     refetchInterval: 5000,
     onError: (err) => {
@@ -60,10 +39,10 @@ export const useAssignQueue = () => {
   const labelerAgent = useLabelerAgent()
   const queryClient = useQueryClient()
   return useMutation(
-    async (input: { did: string; queueId: number }) => {
+    async (input: ToolsOzoneQueueAssignModerator.InputSchema) => {
       const { data } =
         await labelerAgent.tools.ozone.queue.assignModerator(input)
-      return data as AssignmentView
+      return data as ToolsOzoneQueueDefs.AssignmentView
     },
     {
       onSuccess: () => {
@@ -76,14 +55,53 @@ export const useAssignQueue = () => {
   )
 }
 
+export const useReportAssignments = (
+  params: ToolsOzoneReportGetAssignments.QueryParams,
+) => {
+  const labelerAgent = useLabelerAgent()
+  return useQuery({
+    queryKey: [ASSIGNMENTS_QUERY_KEY, params],
+    queryFn: async () => {
+      const { data } =
+        await labelerAgent.tools.ozone.report.getAssignments(params)
+      return (data as { assignments: ToolsOzoneReportDefs.AssignmentView[] })
+        .assignments
+    },
+    refetchInterval: 5000,
+    onError: (err) => {
+      toast.error(`Failed to load assignments:\n${err}`)
+    },
+  })
+}
+
 export const useAssignReport = () => {
   const labelerAgent = useLabelerAgent()
   const queryClient = useQueryClient()
   return useMutation(
-    async (input: { reportId: number; queueId?: number; assign: boolean }) => {
+    async (input: ToolsOzoneReportAssignModerator.InputSchema) => {
       const { data } =
         await labelerAgent.tools.ozone.report.assignModerator(input)
-      return data as AssignmentView
+      return data as ToolsOzoneReportDefs.AssignmentView
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries([ASSIGNMENTS_QUERY_KEY])
+      },
+      onError: (err) => {
+        toast.error(displayError(err))
+      },
+    },
+  )
+}
+
+export const useUnassignReport = () => {
+  const labelerAgent = useLabelerAgent()
+  const queryClient = useQueryClient()
+  return useMutation(
+    async (input: ToolsOzoneReportUnassignModerator.InputSchema) => {
+      const { data } =
+        await labelerAgent.tools.ozone.report.unassignModerator(input)
+      return data as ToolsOzoneReportDefs.AssignmentView
     },
     {
       onSuccess: () => {
@@ -112,7 +130,6 @@ export const useAutoAssignReport = ({
       await labelerAgent.tools.ozone.report.assignModerator({
         reportId,
         queueId,
-        assign: true,
       })
     } catch (err) {
       console.warn(`Auto-assign failed. `, err)
