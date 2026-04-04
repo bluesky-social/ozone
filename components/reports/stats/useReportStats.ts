ab1cbd1377b9b54379e7a8915eddfd4eef325164
gcwill70 <gcwill70@gmail.com>
2026-04-04 12:49:00 -0400

    remove stats mocking

--- components/reports/stats/useReportStats.ts
+++ components/reports/stats/useReportStats.ts
@@ -1,9 +1,6 @@
 import { useLabelerAgent } from '@/shell/ConfigurationContext'
-import { useQuery } from '@tanstack/react-query'
 import { ToolsOzoneReportDefs } from '@atproto/api'
-import { makeHistoricalStats, makeLiveStats } from './mock-stats'
-
-const USE_MOCKS = false
+import { useQuery } from '@tanstack/react-query'
 
 export type LiveStatsParams = {
   queueId?: number
@@ -26,14 +23,12 @@ export const useLiveStats = (params?: LiveStatsParams) => {
 
   return useQuery({
     queryKey: ['report', 'getLiveStats', params],
-    queryFn: USE_MOCKS
-      ? async () => makeLiveStats(params)
-      : async () => {
-          const { data } = await labelerAgent.tools.ozone.report.getLiveStats(
-            params ?? {},
-          )
-          return data.stats
-        },
+    queryFn: async () => {
+      const { data } = await labelerAgent.tools.ozone.report.getLiveStats(
+        params ?? {},
+      )
+      return data.stats
+    },
     refetchInterval: 5 * 60 * 1000,
   })
 }
@@ -43,17 +38,14 @@ export const useHistoricalStats = (params?: HistoricalStatsParams) => {
 
   return useQuery({
     queryKey: ['report', 'getHistoricalStats', params],
-    queryFn: USE_MOCKS
-      ? async () => makeHistoricalStats(params)
-      : async () => {
-          const { data } =
-            await labelerAgent.tools.ozone.report.getHistoricalStats(
-              params ?? {},
-            )
-          return {
-            stats: data.stats as ToolsOzoneReportDefs.HistoricalStats[],
-            cursor: data.cursor,
-          }
-        },
+    queryFn: async () => {
+      const { data } = await labelerAgent.tools.ozone.report.getHistoricalStats(
+        params ?? {},
+      )
+      return {
+        stats: data.stats as ToolsOzoneReportDefs.HistoricalStats[],
+        cursor: data.cursor,
+      }
+    },
   })
 }
