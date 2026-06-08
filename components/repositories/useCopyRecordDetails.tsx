0c34c00dc804fd5e0fd17b5081ae150af3aa3601
Michael Black <michael.black@blueskyweb.xyz>
2026-06-08 09:01:20 -0500

    Add support for new gallery embed on posts (#512)

--- components/repositories/useCopyRecordDetails.tsx
+++ components/repositories/useCopyRecordDetails.tsx
@@ -1,10 +1,23 @@
 import { copyToClipboard } from '@/common/CopyButton'
 import {
+  AppBskyEmbedGallery,
   AppBskyEmbedImages,
   AppBskyEmbedRecordWithMedia,
   asPredicate,
   ToolsOzoneModerationDefs,
 } from '@atproto/api'
+
+const galleryAltsText = (gallery: AppBskyEmbedGallery.Main): string => {
+  let text = ''
+  let i = 1
+  for (const item of gallery.items) {
+    if (AppBskyEmbedGallery.isImage(item)) {
+      text += `Image ${i} ALT: ${item.alt}\n`
+      i += 1
+    }
+  }
+  return text
+}
 
 export const useCopyRecordDetails = ({
   record,
@@ -16,19 +29,25 @@ export const useCopyRecordDetails = ({
     if (record?.value) {
       data += `Created At: ${record.value.createdAt}\n`
       data += `Content: ${record.value.text}\n`
-      if (asPredicate(AppBskyEmbedImages.validateMain)(record.value.embed)) {
-        record.value.embed.images.forEach((img, i) => {
+      const embed = record.value.embed
+      if (asPredicate(AppBskyEmbedImages.validateMain)(embed)) {
+        embed.images.forEach((img, i) => {
           data += `Image ${i + 1} ALT: ${img.alt}\n`
         })
+      } else if (asPredicate(AppBskyEmbedGallery.validateMain)(embed)) {
+        data += galleryAltsText(embed)
       } else if (
-        asPredicate(AppBskyEmbedRecordWithMedia.validateMain)(
-          record.value.embed,
-        ) &&
-        asPredicate(AppBskyEmbedImages.validateMain)(record.value.embed.media)
+        asPredicate(AppBskyEmbedRecordWithMedia.validateMain)(embed)
       ) {
-        record.value.embed.media.images.forEach((img, i) => {
-          data += `Image ${i + 1} ALT: ${img.alt}\n`
-        })
+        if (asPredicate(AppBskyEmbedImages.validateMain)(embed.media)) {
+          embed.media.images.forEach((img, i) => {
+            data += `Image ${i + 1} ALT: ${img.alt}\n`
+          })
+        } else if (
+          asPredicate(AppBskyEmbedGallery.validateMain)(embed.media)
+        ) {
+          data += galleryAltsText(embed.media)
+        }
       }
     }
     copyToClipboard(data, 'record details')
--- components/workspace/hooks.tsx
+++ components/workspace/hooks.tsx
@@ -14,6 +14,7 @@ import {
 import { regenerateBatchId } from '@/lib/batchId'
 import { useServerConfig } from '@/shell/ConfigurationContext'
 import {
+  AppBskyEmbedGallery,
   AppBskyEmbedImages,
   AppBskyEmbedRecord,
   asPredicate,
@@ -267,6 +268,12 @@ const getEmbedValues = (embed: unknown): string => {
       .map(({ image }) => {
         return image.ref.toString()
       })
+      .join('|')
+  }
+  if (asPredicate(AppBskyEmbedGallery.validateMain)(embed)) {
+    return embed.items
+      .filter(AppBskyEmbedGallery.isImage)
+      .map(({ image }) => image.ref.toString())
       .join('|')
   }
   if (asPredicate(AppBskyEmbedRecord.validateMain)(embed)) {
